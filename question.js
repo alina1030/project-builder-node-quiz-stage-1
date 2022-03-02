f321ab5eb439216ac6bdb2e3948f18c19fd51bfb
alina1030 <85539852+alina1030@users.noreply.github.com>
2022-03-02 17:28:44 +0530

    uploaded

---
+++ question.js
@@ -0,0 +1,24 @@
+const quiz=()=>{
+    return({
+        Question: "Silver Beach is located in which Indian State?",
+        Options: {
+           A: "Goa",
+           B: "Kerala",
+           C: "Karnataka",
+           D: "Tamil Nadu"
+        },
+        Answer: "Tamil Nadu"
+    },
+
+    { Question: "Which is the longest river in the world?",
+        Options: {
+           A: "Yellow River",
+           B: "Ganges River",
+           C: "Danube River",
+           D: "Nile River"
+        },
+        Answer: "Nile River"
+
+    })
+}
+module.exports = quiz
